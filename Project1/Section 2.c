81acbcafc62cdf6782dae7cc6615577f66a818a2
Liban Nur <umuliban113@hotmail.com>
2023-09-22 13:07:25 -0400

    Pasted multi print statements

--- Project1/Section 2.c
+++ Project1/Section 2.c
@@ -5,5 +5,13 @@ int main()
 	printf("Hello World!");
 	printf("Date: Sept/22/2023");
 
+	printf("Hello World!");
+	printf("Date: Sept/22/2023");
+
+	printf("Hello World!");
+	printf("Date: Sept/22/2023");
+	printf("helo");
+
+
 	return 0;
 }
